6144b06a592ea0b6a299acbc0d256303708da8c6
kamangele1 <ivanskyunguti@gmail.com>
2023-03-28 16:57:50 -0700

    update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,9 +2,9 @@
 
 """Define a class square."""
 
+
 class Square:
     """Represent a square."""
-
 
     def __init__(self, size=0):
         """Initialize a new square.
@@ -12,7 +12,7 @@ class Square:
             size (int): The size of the new square.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,9 +2,9 @@
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
-
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
@@ -14,10 +14,12 @@ class Square:
         """
         self.size = size
         self.position = position
+
     @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -25,6 +27,11 @@ class Square:
             elif value < 0:
                 raise ValueError("size must be >= 0")
         self.__size = value
+    @property
+    def position(self):
+        """Get/ set the current position of the square."""
+        return (self.__position)
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
@@ -35,6 +42,7 @@ class Square:
         self.__position = value
 
     def area(self):
+
         """Return the current area of the square."""
         return (self.__size * self.__size)
     def my_print(self):
